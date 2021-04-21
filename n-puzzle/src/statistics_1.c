b271bf19b92256fb8b1455d764ef6e1d3b397c8a
Juhani Kauppi <jkauppi@student.hive.fi>
2021-04-21 17:05:46 +0300

    Timestamp support MAX OS

--- n-puzzle/src/statistics_1.c
+++ n-puzzle/src/statistics_1.c
@@ -6,17 +6,27 @@
 /*   By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 23:44:06 by jkauppi           #+#    #+#             */
-/*   Updated: 2021/04/08 10:10:52 by jkauppi          ###   ########.fr       */
+/*   Updated: 2021/04/21 16:28:55 by jkauppi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "n_puzzle.h"
 
+static void	get_time(struct timespec *time)
+{
+#if DARWIN
+	clock_gettime(CLOCK_REALTIME, time);
+#else
+	clock_gettime(_POSIX_MONOTONIC_CLOCK, time);
+#endif
+	return ;
+}
+
 void	stat_set_start_time(t_statistics *statistics)
 {
 	double		ms;
 
-	clock_gettime(_POSIX_MONOTONIC_CLOCK, &statistics->start_time);
+	get_time(&statistics->start_time);
 	statistics->start_time_ms = statistics->start_time.tv_sec * 1000;
 	ms = round(statistics->start_time.tv_nsec / 1.0e6);
 	if (ms > 999)
@@ -32,7 +42,7 @@ void	stat_set_end_time(t_statistics *statistics)
 {
 	double		ms;
 
-	clock_gettime(_POSIX_MONOTONIC_CLOCK, &statistics->end_time);
+	get_time(&statistics->end_time);
 	statistics->end_time_ms = statistics->end_time.tv_sec * 1000;
 	ms = round(statistics->end_time.tv_nsec / 1.0e6);
 	if (ms > 999)
